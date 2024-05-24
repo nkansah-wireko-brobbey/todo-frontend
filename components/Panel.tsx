db85f899f005f354864de70b38cc0d195a5ec973
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-24 15:04:19 -0500

    feat: Add Panel component

---
+++ components/Panel.tsx
@@ -0,0 +1,33 @@
+'use client'
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import { Plus } from 'lucide-react';
+import { useDispatch } from 'react-redux';
+import { openDrawer } from '@/store/reducers/drawer.reducer';
+
+
+export default () => {
+
+    const dispatch = useDispatch();
+
+    const openDrawerHandler = () => {
+        dispatch(openDrawer());
+    }
+  return (
+    <div className='panel'>
+        <div className="title">
+            <div>In Progress</div>
+            <div>
+            <Button variant="ghost" size="icon" className='rounded-full h-7 w-7'
+            onClick={openDrawerHandler}
+            ><Plus size={12}/></Button>
+
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+
