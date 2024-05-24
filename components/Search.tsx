2bbb2c75cd1364f7669d39aff6576d0a3a045e75
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-24 15:04:29 -0500

    feat: Add Search component

---
+++ components/Search.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Input } from "@/components/ui/input"
+
+
+const Search = () => {
+  return (
+    <div>
+        <Input className='rounded-3xl py-1 h-7' />
+    </div>
+  )
+}
+
+export default Search
