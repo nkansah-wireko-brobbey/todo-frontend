2eb038f512c66342b421a3df6fef84a55c5a8b73
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-24 15:01:39 -0500

    chore: Add Account component
    This commit adds the Account component, which includes an avatar and a dropdown menu for user account options.

---
+++ components/Account.tsx
@@ -0,0 +1,36 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  
+const Account = () => {
+  return (
+    <div className="cursor-pointer">
+        <DropdownMenu>
+  <DropdownMenuTrigger>
+
+        <Avatar className="h-8 w-8">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+   
+  </DropdownMenuContent>
+</DropdownMenu>
+
+    </div>
+  )
+}
+
+export default Account
