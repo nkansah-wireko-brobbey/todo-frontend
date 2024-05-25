1d9e1b75312361a746e3460e6d104f030a181d23
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-25 17:44:41 -0500

    Added the panel slice to the store configuration, allowing for state management of panels.

--- store/store.ts
+++ store/store.ts
@@ -1,12 +1,16 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
-import drawerReducer from '@/store/reducers/drawer.reducer';
+import drawerReducer from '@/store/reducers/drawer.slice';
+import panelSlice from './reducers/panel.slice';
 
 const store = configureStore({
   reducer: {
     drawer: drawerReducer,
+    panels: panelSlice
   },
 });
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
 
