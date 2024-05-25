996ab424c0cbccf8646c9174266c88d176f5b2d9
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-25 17:49:38 -0500

    feat: Update Board component to fetch and display panels from Redux store

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,28 +1,46 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Panel from './Panel'
 import AddTodoDrawer from './AddTodoDrawer'
 import { useQuery } from '@apollo/client'
 import { GET_TODOS, GET_CATEGORIES } from '@/lib/graphql/queries'
 import { BoardLoading } from './Loading'
+import { useDispatch } from 'react-redux'
+import { fetchPanels } from '@/store/reducers/panel.slice'
+import { useAppDispatch, useAppSelector } from '@/lib/hooks/reduxStore.hooks'
 
 const Board = () => {
   const { loading, error, data } = useQuery(GET_CATEGORIES);
 
   console.log(data)
 
+
+  const dispatch = useAppDispatch();
+
+  const panels: any[] = useAppSelector((state) => state.panels.data);
+
+  console.log(panels)
+
+  useEffect(() => {  
+      dispatch(fetchPanels())    
+  }, [])
+
+
+
   return (
     <>
     {loading && <BoardLoading/>}
 
 
-    {!loading && 
+    {!loading && data &&
     <><div className='board'>
-          <Panel />
-          <Panel />
-          <Panel />
-          <Panel />
+          {
+            panels.map((panel: any) => {
+              return <Panel key={panel.id} title={panel.name} id={panel.id} todos={panel.todos} />
+            })
+          }
+          
         </div><AddTodoDrawer /></>
     
     }
