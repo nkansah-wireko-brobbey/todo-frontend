f07b7d3ccb87f19912c1946f053dbeb003057e47
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-25 11:07:40 -0500

    Added loading skeleton for board component

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,17 +1,31 @@
+'use client'
+
 import React from 'react'
 import Panel from './Panel'
 import AddTodoDrawer from './AddTodoDrawer'
+import { useQuery } from '@apollo/client'
+import { GET_TODOS, GET_CATEGORIES } from '@/lib/graphql/queries'
+import { BoardLoading } from './Loading'
 
 const Board = () => {
+  const { loading, error, data } = useQuery(GET_CATEGORIES);
+
+  console.log(data)
+
   return (
     <>
-    <div className='board'>
-        <Panel />
-        <Panel />
-        <Panel />
-        <Panel />
-    </div>
-    <AddTodoDrawer />
+    {loading && <BoardLoading/>}
+
+
+    {!loading && 
+    <><div className='board'>
+          <Panel />
+          <Panel />
+          <Panel />
+          <Panel />
+        </div><AddTodoDrawer /></>
+    
+    }
     </>
 
   )
