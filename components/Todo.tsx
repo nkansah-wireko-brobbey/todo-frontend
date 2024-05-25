131ab872d328b243d414214ad505807e8633f96e
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-25 17:50:03 -0500

    refactor: Update Todo component to accept data prop and display title

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -8,18 +8,33 @@ import {
   } from "@/components/ui/card"
   import { Badge } from "@/components/ui/badge"
 
+interface TodoData{
+    title: string;
+    id: number;
+    date: Date;
+    category: {
+        name?: string;
+    }
+}
 
+type Props = {
+  data: TodoData
+}
   
-const Todo = () => {
+const Todo = ({data}: Props) => {
+
+  console.log("Todo data",data)
   return (
     <div>
         <Card>
   <CardHeader className="text-sm p-2">
-    <CardTitle className="text-sm"><Badge variant="secondary" className="bg-green-200 text-green-700">Badge</Badge>
+    <CardTitle className="text-sm">
+      <Badge variant="secondary" className="bg-green-200 text-green-700">{data.category?.name || "Not Assigned"}</Badge>
 </CardTitle>
   </CardHeader>
   <CardContent className="text-sm p-2">
-    <p>Card Content Card Content</p>
+    <p>      {data.title}
+</p>
   </CardContent>
   <CardFooter className="text-sm p-2">
     
