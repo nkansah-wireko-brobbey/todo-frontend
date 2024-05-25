9b3dd866abf86f436ce884b720b8fc84c1ebdd14
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-25 17:48:45 -0500

    Added responsive modal implementation for drawer

--- components/AddTodoDrawer.tsx
+++ components/AddTodoDrawer.tsx
@@ -1,5 +1,9 @@
 'use client'
-import React from 'react'
+import * as React from "react"
+ 
+import { cn } from "@/lib/utils"
+import { useMediaQuery } from "usehooks-ts";
+import { Button } from "@/components/ui/button"
 import {
     Drawer,
     DrawerClose,
@@ -10,9 +14,19 @@ import {
     DrawerTitle,
     DrawerTrigger,
   } from "@/components/ui/drawer"
-import { Button } from "@/components/ui/button"
+
+  import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
 import { useDispatch, useSelector } from 'react-redux'
-import { closeDrawer } from '@/store/reducers/drawer.reducer'
+import { closeDrawer } from '@/store/reducers/drawer.slice'
   
 
 const AddTodoDrawer = () => {
@@ -24,6 +38,33 @@ const AddTodoDrawer = () => {
     const closeDrawerHandler = () => {
       dispatch(closeDrawer());
     }
+    const isDesktop = useMediaQuery("(min-width: 768px)")
+
+    if (isDesktop) {
+      return (
+        <Dialog open={isDrawerOpen}>
+          {/* <DialogTrigger asChild>
+            <Button variant="outline">Edit Profile</Button>
+          </DialogTrigger> */}
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            {/* <ProfileForm /> */}
+
+            <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+          <Button variant="outline" onClick={closeDrawerHandler}>Cancel</Button>
+          </DialogClose>
+        </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      )
+    }
+
   return (
     <div>
         <Drawer
