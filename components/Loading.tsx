d2c630cf27170ed1fdce22d2a83e6ee28af02b9c
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-25 17:49:05 -0500

    feat: Add loading components for board and panel

---
+++ components/Loading.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton"
+
+
+export const BoardLoading = () => {
+  return (
+    <div>
+        <div className="board flex justify-between">
+        <CardLoading />
+        <CardLoading />
+        <CardLoading />
+        <CardLoading />
+    </div>
+    </div>
+  )
+}
+export const PanelLoading = () => {
+  return (
+    <div>Loading</div>
+  )
+}
+
+const CardLoading = () => {
+    return (
+        <div>   
+        <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+  
+          </div></div>
+    )
+    }
+
