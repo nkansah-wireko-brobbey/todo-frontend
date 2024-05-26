437ac842fa7588157f6a3b5b8fc0afcb9b701083
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-26 13:14:25 -0500

    Implemented delete functionality

--- components/TodoContextMenu.tsx
+++ components/TodoContextMenu.tsx
@@ -5,16 +5,64 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
+    DropdownMenuSub,
+    DropdownMenuSubTrigger,
+    DropdownMenuSubContent,
+    DropdownMenuPortal,
   } from "@/components/ui/dropdown-menu"
+import { UserPlus, Mail, MessageSquare, PlusCircle } from "lucide-react"
 
   import { Button } from "@/components/ui/button"
 import { Ellipsis } from "lucide-react"
-  
+import { useEffect } from "react"
+import { fetchCategories } from "@/store/reducers/category.slice"
+import { fetchPanels } from "@/store/reducers/panel.slice"
+import { useAppDispatch } from "@/lib/hooks/reduxStore.hooks"
+import { useAppSelector } from "@/lib/hooks/reduxStore.hooks"  
+import { useMutation } from "@apollo/client"
+import { DELETE_TODO } from "@/lib/graphql/mutations"
+import client from '@/lib/apolloClient.config'
+
 type Props ={
     todos_id: number
 }
 
 const TodoContextMenu = ({todos_id}:Props) => {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(fetchCategories())
+        dispatch(fetchPanels())  
+
+      }
+      ,[])
+
+      let {data:categories} = useAppSelector((state) => state.categories);
+      let panels: any[] = useAppSelector((state) => state.panels.data);
+
+      const [mutateFunction, { loading, error, data }] = useMutation(DELETE_TODO);
+
+
+      const deleteTodoHandler = async () => {
+            try {
+                console.log("Variable: ", todos_id)
+                const { data } = await client.mutate({
+                    mutation: DELETE_TODO,
+                    variables: {
+                      id: todos_id
+                    },
+                  });
+ 
+                console.log("Data", data)
+            }
+            catch (error) {
+                console.log("Error", error)
+            }
+            finally {
+                console.log("Finally")
+            }
+        }
+
+
   return (
     <div><DropdownMenu>
     <DropdownMenuTrigger><div>
@@ -24,12 +72,29 @@ const TodoContextMenu = ({todos_id}:Props) => {
 
             </div></DropdownMenuTrigger>
     <DropdownMenuContent>
-      <DropdownMenuLabel>Menu</DropdownMenuLabel>
+      <DropdownMenuLabel>Update Panels</DropdownMenuLabel>
       <DropdownMenuSeparator />
-      <DropdownMenuItem>Profile</DropdownMenuItem>
-      <DropdownMenuItem>Billing</DropdownMenuItem>
-      <DropdownMenuItem>Team</DropdownMenuItem>
-      <DropdownMenuItem><span className="font-semibold text-red-700">Delete</span></DropdownMenuItem>
+      {     panels &&
+            panels.map((panel) => {
+                return <DropdownMenuItem key={panel.id}>{panel.name}</DropdownMenuItem>
+            })
+      }
+      <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <span>Update Category</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+              { categories &&
+            categories.map((category) => {
+                return <DropdownMenuItem key={category.id}>{category.name}</DropdownMenuItem>
+            })
+            }
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+     
+      <DropdownMenuItem onClick={deleteTodoHandler}><span className="font-semibold text-red-700">Delete</span></DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
   </div>
