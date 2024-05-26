653bfe3fb2971f00d8b8ed63531d1bd8b0147f70
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-26 02:09:59 -0500

    Added category slice for managing category state

--- store/store.ts
+++ store/store.ts
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import drawerReducer from '@/store/reducers/drawer.slice';
 import panelSlice from './reducers/panel.slice';
 import userSlice from './reducers/user.slice';
+import categorySlice from './reducers/category.slice';
 
 const store = configureStore({
   reducer: {
     drawer: drawerReducer,
     panels: panelSlice,
-    user: userSlice
+    user: userSlice,
+    categories: categorySlice
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
