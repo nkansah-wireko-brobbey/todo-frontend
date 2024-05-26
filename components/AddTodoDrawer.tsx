f5dca3c2f67d484e83e62c390ed6ca86ac162682
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-26 02:08:05 -0500

    refactor: Update AddTodoDrawer component to use redux hooks and improve code organization

--- components/AddTodoDrawer.tsx
+++ components/AddTodoDrawer.tsx
@@ -27,13 +27,29 @@ import {
   } from "@/components/ui/dialog"
 import { useDispatch, useSelector } from 'react-redux'
 import { closeDrawer } from '@/store/reducers/drawer.slice'
-  
+import { useAppDispatch } from '@/lib/hooks/reduxStore.hooks'
+import { useAppSelector } from "@/lib/hooks/reduxStore.hooks";
 
-const AddTodoDrawer = () => {
+import { AddTodoForm, TODO_FORM } from './AddTodoForm'
+import { CATEGORY_FORM, AddTodoFormCategory } from "./AddTodoFormCategory";
+
+
+
+type Props = {
+    title?: string;
+    description?: string;
+    FormComponent?: any;
+}
+
+const AddTodoDrawer = ({title, description, FormComponent}:Props) => {
   
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
 
     const isDrawerOpen = useSelector((state:any) => state.drawer.isDrawerOpen);
+
+    let {componentName, componentTitle, componentDescription, panel_id} = useAppSelector((state:any) => state.drawer);
+
+
   
     const closeDrawerHandler = () => {
       dispatch(closeDrawer());
@@ -43,21 +59,20 @@ const AddTodoDrawer = () => {
     if (isDesktop) {
       return (
         <Dialog open={isDrawerOpen}>
-          {/* <DialogTrigger asChild>
-            <Button variant="outline">Edit Profile</Button>
-          </DialogTrigger> */}
+          {}
           <DialogContent className="sm:max-w-[425px]">
             <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
+              <DialogTitle>{componentTitle || "Edit profile"}</DialogTitle>
               <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
+               {componentDescription ||" Make changes to your profile here. Click save when you're done."}
               </DialogDescription>
             </DialogHeader>
-            {/* <ProfileForm /> */}
+            { (componentName === TODO_FORM) && <AddTodoForm panel_id={panel_id}/>}
+            {(componentName === CATEGORY_FORM) && <AddTodoFormCategory />}
 
             <DialogFooter className="sm:justify-start">
           <DialogClose asChild>
-          <Button variant="outline" onClick={closeDrawerHandler}>Cancel</Button>
+          <Button variant="outline" onClick={closeDrawerHandler}>Close</Button>
           </DialogClose>
         </DialogFooter>
           </DialogContent>
@@ -66,18 +81,18 @@ const AddTodoDrawer = () => {
     }
 
   return (
-    <div>
         <Drawer
         open={isDrawerOpen}
         >
   {/* <DrawerTrigger>Open</DrawerTrigger> */}
   <DrawerContent className='flex flex-col items-center'>
     <DrawerHeader>
-      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-      <DrawerDescription>This action cannot be undone.</DrawerDescription>
+      <DrawerTitle>{componentTitle}</DrawerTitle>
+      <DrawerDescription>{componentDescription}</DrawerDescription>
     </DrawerHeader>
+    {(componentName === TODO_FORM) && <AddTodoForm panel_id={panel_id} />}
+    {(componentName === CATEGORY_FORM) && <AddTodoFormCategory />}
     <DrawerFooter>
-      <Button>Submit</Button>
       <DrawerClose>
         <Button variant="outline" onClick={closeDrawerHandler}>Cancel</Button>
       </DrawerClose>
@@ -85,7 +100,6 @@ const AddTodoDrawer = () => {
   </DrawerContent>
 </Drawer>
 
-    </div>
   )
 }
 
