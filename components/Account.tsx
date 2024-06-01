986f2851e1187c1dc779b2ab574bc77419034fae
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-31 23:17:18 -0500

    refactor: Update Account component to use Clerk authentication

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,36 +1,19 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
-  
+"use client";
+
+import { SignedIn, UserButton, SignedOut, SignInButton } from "@clerk/clerk-react";
+
+
 const Account = () => {
   return (
     <div className="cursor-pointer">
-        <DropdownMenu>
-  <DropdownMenuTrigger asChild>
-
-        <Avatar className="h-8 w-8">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-
-  </DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-   
-  </DropdownMenuContent>
-</DropdownMenu>
-
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
     </div>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
